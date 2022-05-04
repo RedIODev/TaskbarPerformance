2d7168ea8d23af021d79a6f58e348422de0ba7d5
RedIO <redio.development@gmail.com>
2022-05-04 17:40:57 +0200

    initial commit

---
+++ PerformanceWindow.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+namespace TaskbarPerformance
+{
+    public partial class PerformanceWindow : Form
+    {
+        private static readonly TaskbarManager tbm = TaskbarManager.Instance;
+        private readonly Timer timer = new() { Interval = 1000, Enabled = true };
+
+        public delegate TaskbarProgressBarState StateHandler(sbyte value);
+        public delegate sbyte StateProvider();
+        public sbyte MaxValue { get; } = 100;
+        public int UpdateInterval { get => timer.Interval; set => timer.Interval = value; }
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+        public StateHandler Handler { get; set; }
+        public StateProvider Provider { get; set; }
+
+        public TaskbarProgressBarState ProgressBarState { set {
+                if (IsDisposed)
+                    return;
+                tbm.SetProgressState(value, Handle);
+            }
+        }
+
+        public sbyte ProgressBarValue { set {
+                ProgressBarState = Handler(value);
+                tbm.SetProgressValue(value, MaxValue, Handle);
+            }
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+            sbyte value = Provider();
+            ProgressBarValue = value;
+            Text = $"{Prefix}{value}{Suffix}";
+        }
+
+        public PerformanceWindow(StateHandler handler, StateProvider provider)
+        {
+            InitializeComponent();
+            Handler = handler;
+            Provider = provider;
+            timer.Tick += Timer_Tick;
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Taskbar;
 
 namespace TaskbarPerformance
 {
     static class Program
     {
+        private static PerformanceCounter perCounter = new PerformanceCounter() { CategoryName = "Processor", CounterName= "% Processor time", InstanceName = "_Total"};
+        //private static Random rand = new();
+        public static readonly PerformanceWindow pwin;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
+        /// 
+
+        static Program()
         {
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            pwin = new(val => val switch
+            {
+                100 => TaskbarProgressBarState.Error,
+                > 75 => TaskbarProgressBarState.Paused,
+                _ => TaskbarProgressBarState.Normal,
+            }, () => (sbyte)perCounter.NextValue()
+            )
+            { 
+                Prefix = "CPU: ",
+                Suffix = " %"
+            };
+        }
+        [STAThread]
+        static void Main()
+        {
+        
+            Application.Run(pwin);
+
         }
     }
 }
